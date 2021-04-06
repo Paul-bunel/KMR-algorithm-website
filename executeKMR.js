13395bbee088f0b302b241c3e12cc0d16de73c2f
Paul-bunel <paulbunel34@gmail.com>
2021-04-06 19:07:49 +0200

    Modification de l'algorithme pour pouvoir récupérer tous les motifs calculés

--- executeKMR.js
+++ executeKMR.js
@@ -1,7 +1,13 @@
 const validationButton = document.body.querySelector('#validation-button');
+const sequenceInput = document.body.querySelector('#sequence');
+const printSequence = document.body.querySelector('#print-sequence');
+const algoResults = document.body.querySelector('#algo-results');
 
 validationButton.onclick = applyKMR;
 
 function applyKMR(event) {
-
+    let sequence = sequenceInput.value;
+    kmr = new KMR(sequence);
+    printSequence.innerText = `La sequence que vous avez choisi : ${sequence}`;
+    algoResults.innerHTML = kmr.toHTML();
 }
